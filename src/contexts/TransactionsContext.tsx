434aefc13a4ad53beb4df51ff9a9b85bea3a96ca
ssisaias <ssisaias@users.noreply.github.com>
2024-03-10 18:42:08 -0300

    Install and use Axios instead of fetch

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useState } from "react";
+import { api } from "../lib/axios";
 
 export interface Transaction {
   id: number;
@@ -23,13 +24,13 @@ interface TransactionsProviderProps {
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  async function fetchTransactions( query?: string) {
-    const url = new URL("http://localhost:3333/transactions");
-    if (query) {
-      url.searchParams.append("q", query);
-    }
-    const response = await fetch(url);
-    const data = await response.json();
+  async function fetchTransactions(query?: string) {
+    const response = api.get(`/transactions`, {
+      params: {
+        q: query,
+      },
+    });
+    const { data } = await response;
     setTransactions(data);
   }
 
