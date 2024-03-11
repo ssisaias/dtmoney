670f39875a5fe0068b6497dab850df9fb2073791
ssisaias <ssisaias@users.noreply.github.com>
2024-03-10 23:54:57 -0300

    Sample of the useMemo
    -use Memo is used to memoize the value returned by functions and
    only computate them again when the dependency changes
    (whereas useCallback does something similar but to the whole function instead)

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,26 +1,35 @@
 import { useContextSelector } from "use-context-selector";
-import { Transaction, TransactionsContext } from "../contexts/TransactionsContext";
+import {
+  Transaction,
+  TransactionsContext,
+} from "../contexts/TransactionsContext";
+import { useMemo } from "react";
 
 export function useSummary() {
-  const { transactions } = useContextSelector(TransactionsContext, (state) => state);
-
-  const summary = transactions.reduce(
-    (acc, transaction: Transaction) => {
-      if (transaction.type === "income") {
-        acc.income += transaction.price;
-        acc.balance += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.balance -= transaction.price;
-      }
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      balance: 0,
-    }
+  const { transactions } = useContextSelector(
+    TransactionsContext,
+    (state) => state
   );
+    // useMemo works similar to memo, but it is a hook instead, used to make a function only runs again when a dependecy of that function changes
+  const summary = useMemo(() => {
+    transactions.reduce(
+      (acc, transaction: Transaction) => {
+        if (transaction.type === "income") {
+          acc.income += transaction.price;
+          acc.balance += transaction.price;
+        } else {
+          acc.outcome += transaction.price;
+          acc.balance -= transaction.price;
+        }
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        balance: 0,
+      }
+    );
+  }, [transactions]);
 
   return summary;
 }
